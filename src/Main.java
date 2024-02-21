c93199128b1cb4448811e9ae50a2f640c04573ed
Salem <salo0002@stud.kea.dk>
2024-02-21 12:18:03 +0100

    3rd commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
 import java.util.Scanner;
 
-        public class Main {
+public class Main {
 
-            public static void main(String[] args) {
+    public static void main(String[] args) {
 
-                Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
+        int opret = 0;
+        int afslut = 2;
+
+        while (opret != afslut) {
+
+            System.out.println("Velkommen til min filmsamnling!");
+            System.out.println("1 : Opret en film");
+            System.out.println("2 : Afslut");
+            opret = scanner.nextInt();
+
+            if (opret == 1) {
                 System.out.println("indtast filmens navn: ");
                 String filmNavn = scanner.nextLine();
                 System.out.println("indtast filmens direktør");
@@ -18,19 +30,22 @@ import java.util.Scanner;
                 String filmGenre = scanner.nextLine();
                 System.out.println("Er den farvede, så skriv yes");
                 String farvede = scanner.next();
-                boolean erDenFarvede =false;
-                if (farvede.equals("yes")){
+                boolean erDenFarvede = false;
+                if (farvede.equals("yes")) {
                     erDenFarvede = true;
+
+                    Movie movie = new Movie(filmNavn, filmDirektør, filmLængde, filmÅr, filmGenre, erDenFarvede);
+                    System.out.println(movie.getFilmNavn() + " Movie er lavet ");
+
+                    MovieCollection collection = new MovieCollection();
+                    collection.addMovie(movie);
                 }
-
-                Movie movie = new Movie(filmNavn, filmDirektør, filmLængde,filmÅr,filmGenre,erDenFarvede);
-                System.out.println(movie.getFilmDirektør());
-
-                MovieCollection collection = new MovieCollection (); collection.addMovie (movie);
-
-
+            } else if (opret == afslut) {
+                System.out.println("Du valgte ingen film!");
+                break;
             }
-
-
-
         }
+
+        scanner.close();
+    }
+}
